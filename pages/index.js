38b80e9f1600aa75d6f8009bc95848afd2100ccb
adminLTI <admin@lighttherapyinsiders.com>
2025-12-03 10:16:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,3 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/help");
-  }, [router]);
-
-  return <div>Loading chat…</div>;
+  return <div>Go to /help to open the chat.</div>;
 }
