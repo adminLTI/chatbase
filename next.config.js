ddb93d41501bdd13a24c90772b43c3bd1bb031d9
adminLTI <admin@lighttherapyinsiders.com>
2025-12-03 11:01:22 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/help",
+        destination: "https://chatbase.co/mNJrhtPVtz8wUEBvDmUo1/help",
+      },
+      {
+        source: "/help/:path*",
+        destination: "https://chatbase.co/mNJrhtPVtz8wUEBvDmUo1/help/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
